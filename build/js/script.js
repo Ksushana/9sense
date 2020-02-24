cae4b1655b7faa53e0ab5f9b08c675781964b2ae
Ksenia Balashova <utyatka@gmail.com>
2020-02-24 21:30:46 +0100

    text parallax

--- build/js/script.js
+++ build/js/script.js
@@ -9491,9 +9491,16 @@ $(() => {
   let ticking = false;
 
   const pictures = $(".scale--big");
+  const textBlocks = $(".text-block");
 
   pictures.each((_, picture) => {
     $(picture).css("transition", "transform 0.1s");
+  });
+
+  textBlocks.each((_, textBlock) => {
+    $(textBlock)
+      .find("p")
+      .css("transition", "transform 0.1s");
   });
 
   window.addEventListener(`scroll`, () =>
@@ -9509,13 +9516,15 @@ $(() => {
 
   function requestTick() {
     if (!ticking) {
-      requestAnimationFrame(animatePictures);
+      requestAnimationFrame(animate);
       ticking = true;
     }
   }
 
-  function updatePicture(element, { scale }) {
-    element.css("transform", `scale(${scale})`);
+  function animate() {
+    animateTexts();
+    animatePictures();
+    ticking = false;
   }
 
   function animatePictures() {
@@ -9524,7 +9533,10 @@ $(() => {
       const scale = calculateScale(picture);
       updatePicture(picture, { scale });
     });
-    ticking = false;
+  }
+
+  function updatePicture(element, { scale }) {
+    element.css("transform", `scale(${scale})`);
   }
 
   function calculateScale(element) {
@@ -9542,6 +9554,34 @@ $(() => {
     const scale = minScale + (maxScale - minScale) * relative;
 
     return scale;
+  }
+
+  function animateTexts() {
+    textBlocks.each((_, textBlock) => {
+      textBlock = $(textBlock);
+      const relative = calculateTextRelative(textBlock);
+      updateTextBlock(textBlock, relative);
+    });
+  }
+
+  function calculateTextRelative(element) {
+    const scrollTop = lastScroll;
+    const windowHeight = $(window).height();
+
+    const elementOffset = element.offset().top;
+    const elementHeight = element.height();
+
+    const startOffset = elementOffset - windowHeight;
+    const finishOffset = elementOffset - windowHeight / 2 + elementHeight / 2;
+
+    const position = (scrollTop - startOffset) / (finishOffset - startOffset);
+    const relative = Math.max(0, Math.min(1, position));
+    return relative;
+  }
+
+  function updateTextBlock(element, relative) {
+    const offset = relative * -50;
+    element.find("p").css("transform", `translateY(${offset}px)`);
   }
 });
 
--- src/js/modules/sections.js
+++ src/js/modules/sections.js
@@ -6,9 +6,16 @@ $(() => {
   let ticking = false;
 
   const pictures = $(".scale--big");
+  const textBlocks = $(".text-block");
 
   pictures.each((_, picture) => {
     $(picture).css("transition", "transform 0.1s");
+  });
+
+  textBlocks.each((_, textBlock) => {
+    $(textBlock)
+      .find("p")
+      .css("transition", "transform 0.1s");
   });
 
   window.addEventListener(`scroll`, () =>
@@ -24,13 +31,15 @@ $(() => {
 
   function requestTick() {
     if (!ticking) {
-      requestAnimationFrame(animatePictures);
+      requestAnimationFrame(animate);
       ticking = true;
     }
   }
 
-  function updatePicture(element, { scale }) {
-    element.css("transform", `scale(${scale})`);
+  function animate() {
+    animateTexts();
+    animatePictures();
+    ticking = false;
   }
 
   function animatePictures() {
@@ -39,7 +48,10 @@ $(() => {
       const scale = calculateScale(picture);
       updatePicture(picture, { scale });
     });
-    ticking = false;
+  }
+
+  function updatePicture(element, { scale }) {
+    element.css("transform", `scale(${scale})`);
   }
 
   function calculateScale(element) {
@@ -57,5 +69,33 @@ $(() => {
     const scale = minScale + (maxScale - minScale) * relative;
 
     return scale;
+  }
+
+  function animateTexts() {
+    textBlocks.each((_, textBlock) => {
+      textBlock = $(textBlock);
+      const relative = calculateTextRelative(textBlock);
+      updateTextBlock(textBlock, relative);
+    });
+  }
+
+  function calculateTextRelative(element) {
+    const scrollTop = lastScroll;
+    const windowHeight = $(window).height();
+
+    const elementOffset = element.offset().top;
+    const elementHeight = element.height();
+
+    const startOffset = elementOffset - windowHeight;
+    const finishOffset = elementOffset - windowHeight / 2 + elementHeight / 2;
+
+    const position = (scrollTop - startOffset) / (finishOffset - startOffset);
+    const relative = Math.max(0, Math.min(1, position));
+    return relative;
+  }
+
+  function updateTextBlock(element, relative) {
+    const offset = relative * -50;
+    element.find("p").css("transform", `translateY(${offset}px)`);
   }
 });
