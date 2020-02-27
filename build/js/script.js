100d897f38a2e4fbde430f536b3e272ec6aebc58
Ksenia Balashova <utyatka@gmail.com>
2020-02-27 12:07:37 +0100

    fix

--- build/js/script.js
+++ build/js/script.js
@@ -10599,36 +10599,20 @@ $(function() {
 });
 
 $(document).ready(function() {
-    $(".slider__img").magnificPopup({
-      type: "image",
-      closeOnContentClick: true,
-      closeBtnInside: true,
-      fixedContentPos: true,
-      overflowY: scroll;
-      mainClass: "mfp-no-margins mfp-with-zoom", // class to remove default margin from left and right side
-      image: {
-        verticalFit: true
-      },
-      zoom: {
-        enabled: true,
-        duration: 300, // don't foget to change the duration also in CSS
-        easing: 'ease-in-out',
-        opener: function(openerElement) {
-          return openerElement.is('img') ? openerElement : openerElement.find('img');
-        }
-      },
-      retina: {
-        ratio: 1, // Increase this number to enable retina image support.
-        // Image in popup will be scaled down by this number.
-        // Option can also be a function which should return a number (in case you support multiple ratios). For example:
-        // ratio: function() { return window.devicePixelRatio === 1.5 ? 1.5 : 2  }
-
-
-        replaceSrc: function(item, ratio) {
-          return item.src.replace(/\.\w+$/, function(m) { return '@2x' + m; });
-        } // function that changes image source
-      }
-    });
+  $(".slider__img").magnificPopup({
+    type: "image",
+    closeOnContentClick: true,
+    closeBtnInside: false,
+    fixedContentPos: true,
+    mainClass: "mfp-no-margins mfp-with-zoom", // class to remove default margin from left and right side
+    image: {
+      verticalFit: true
+    },
+    zoom: {
+      enabled: true,
+      duration: 300 // don't foget to change the duration also in CSS
+    }
+  });
 });
 
 $(() => {
