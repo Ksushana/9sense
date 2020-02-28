0e64bada93c5aba4de1bb78b72e78d4c05d4fe35
Ksenia Balashova <utyatka@gmail.com>
2020-02-28 10:58:17 +0100

    desktop fix

--- src/js/modules/sliderColor.js
+++ src/js/modules/sliderColor.js
@@ -12,7 +12,7 @@
   window.animateSlider = function(lastScroll) {
     const imgs = document.querySelectorAll(`.slider__slide img`);
     const breakpoint = slider.offset().top + slider.height();
-    const current = lastScroll + $(window).height() / 2 + slider.height() / 1.5;
+    const current = lastScroll + $(window).height() / 2 + slider.height() / 2;
     if (current > breakpoint) {
       for (let i = 0; i < imgs.length; i++) {
         const img = imgs[i];
