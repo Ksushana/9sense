23893a21dccff27f1670f3e19c2d1f76f89c4bc1
Ksenia Balashova <utyatka@gmail.com>
2020-03-03 12:30:54 +0100

    scroll time

--- src/js/global/anchor.js
+++ src/js/global/anchor.js
@@ -3,13 +3,13 @@ $(document).ready(function() {
     event.preventDefault();
     var id = $(this).attr("href"),
       top = $(id).offset().top + $(id).height() / 2 - $(window).height() / 2;
-    $("body,html").animate({ scrollTop: top }, 1500);
+    setTimeout(() => $("body,html").animate({ scrollTop: top }, 1000), 900);
   });
 
   $("#links").on("click", "a.header__link--loc", function(event) {
     event.preventDefault();
     var id = $(this).attr("href"),
       top = $(id).offset().top + $(window).height() / 2;
-    $("body,html").animate({ scrollTop: top }, 1500);
+    setTimeout(() => $("body,html").animate({ scrollTop: top }, 1000), 900);
   });
 });
