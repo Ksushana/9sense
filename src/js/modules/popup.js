b853ef48c6fd18ce605b662dc2eee4d5d4ca5bef
Ksenia Balashova <utyatka@gmail.com>
2021-01-31 18:21:37 +0100

    add swipe to close popup

--- src/js/modules/popup.js
+++ src/js/modules/popup.js
@@ -8,10 +8,10 @@ $(document).ready(function() {
   }
   $(".slider__image").magnificPopup({
     type: "image",
-    closeOnContentClick: true,
     closeBtnInside: false,
     fixedContentPos: true,
     mainClass: "mfp-no-margins mfp-with-zoom",
+    closeOnContentClick: true,
     image: {
       verticalFit: true
     },
@@ -20,11 +20,12 @@ $(document).ready(function() {
       duration: 300
     },
     callbacks: {
-      open: () => scollCenter(0)
+      open: () => scrollCenter(0),
+      imageLoadComplete: () => closeOnSwipe()
     }
   });
 
-  function scollCenter(attempt) {
+  function scrollCenter(attempt) {
     const currentScroll = $(".mfp-figure").scrollLeft();
     if (attempt > 100) {
       return;
@@ -32,7 +33,18 @@ $(document).ready(function() {
 
     $(".mfp-figure").scrollLeft($(window).width() / 2);
     if (currentScroll === 0) {
-      setTimeout(() => scollCenter(attempt + 1), 10);
+      setTimeout(() => scrollCenter(attempt + 1), 10);
     }
   }
+
+  function closeOnSwipe() {
+    $(".mfp-img").swipe( {
+      swipeUp:function() {
+        $.magnificPopup.instance.close()
+      },
+      swipeDown:function() {
+        $.magnificPopup.instance.close()
+      }
+    });
+  };
 });
